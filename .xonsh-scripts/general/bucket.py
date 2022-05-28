eaab16b0f24acda8dc37a44467b17dd933c9ec9f
Andrew Wray <andrewwray2718@gmail.com>
2022-05-28 11:29:53 -0700

    Add bucket type annotation

--- .xonsh-scripts/general/bucket.py
+++ .xonsh-scripts/general/bucket.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 
 import boto3
+from mypy_boto3_s3 import S3ServiceResource
 
 s3 = boto3.resource("s3")
 
@@ -8,7 +9,7 @@ s3 = boto3.resource("s3")
 @dataclass
 class S3Bucket:
     name: str
-    bucket: "Bucket" = field(init=False)
+    bucket: S3ServiceResource = field(init=False)
 
     def __post_init__(self):
         self.bucket = s3.Bucket(self.name)
